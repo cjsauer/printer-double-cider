00d8136b4efd0c9f22a3b9f2c202182d3648facc
Calvin Sauer <calvin.j.sauer@gmail.com>
2018-01-25 00:27:46 -0500

    Implement page-ranges fn

--- src/cljs/printer_double_sider/core.cljs
+++ src/cljs/printer_double_sider/core.cljs
@@ -3,6 +3,13 @@
 
 (enable-console-print!)
 
+(defn page-ranges
+  [n slides-per-page]
+  (let [ranges (map (juxt first last)
+                    (partition-all slides-per-page (range 1 (inc n))))]
+    {:side-one (take-nth 2 ranges)
+     :side-two (take-nth 2 (drop 1 ranges))}))
+
 (defonce app-state (atom {:text "Hello Chestnut!"}))
 
 (defn greeting []
@@ -10,3 +17,9 @@
 
 (defn render []
   (reagent/render [greeting] (js/document.getElementById "app")))
+
+(comment
+
+  (range 41)
+
+  )
