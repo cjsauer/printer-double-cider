c126d67e7adcd85e4b586801db656c2c00275432
Calvin Sauer <calvin.j.sauer@gmail.com>
2018-01-24 23:13:18 -0500

    Upgrade all deps

--- project.clj
+++ project.clj
@@ -1,26 +1,27 @@
 (defproject printer-double-sider "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A helpful tool for people with printers that don't support double-sided printing"
+  :url "https://github.com/cjsauer/printer-double-sider"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.671" :scope "provided"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.9.946" :scope "provided"]
                  [com.cognitect/transit-clj "0.8.300"]
-                 [ring "1.6.2"]
+                 [ring "1.6.3"]
                  [ring/ring-defaults "0.3.1"]
                  [bk/ring-gzip "0.2.1"]
                  [radicalzephyr/ring.middleware.logger "0.6.0"]
                  [compojure "1.6.0"]
                  [environ "1.1.0"]
                  [com.stuartsierra/component "0.3.2"]
-                 [org.danielsz/system "0.4.0"]
+                 [org.danielsz/system "0.4.1"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [reagent "0.6.0"]
-                 [lambdaisland/garden-watcher "0.3.1"]]
+                 [reagent "0.7.0"]
+                 [lambdaisland/garden-watcher "0.3.2"]]
 
-  :plugins [[lein-cljsbuild "1.1.6"]
-            [lein-environ "1.1.0"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-environ "1.1.0"]
+            [lein-ancient "0.6.15"]]
 
   :min-lein-version "2.6.1"
 
@@ -98,15 +99,15 @@
   :doo {:build "test"}
 
   :profiles {:dev
-             {:dependencies [[figwheel "0.5.11"]
-                             [figwheel-sidecar "0.5.11"]
+             {:dependencies [[figwheel "0.5.14"]
+                             [figwheel-sidecar "0.5.14"]
                              [com.cemerick/piggieback "0.2.2"]
                              [org.clojure/tools.nrepl "0.2.13"]
-                             [lein-doo "0.1.7"]
-                             [reloaded.repl "0.2.3"]]
+                             [lein-doo "0.1.8"]
+                             [reloaded.repl "0.2.4"]]
 
-              :plugins [[lein-figwheel "0.5.11"]
-                        [lein-doo "0.1.7"]]
+              :plugins [[lein-figwheel "0.5.14"]
+                        [lein-doo "0.1.8"]]
 
               :source-paths ["dev"]
               :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}
