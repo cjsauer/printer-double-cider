bb23da956a4a6ceabfc7519473dade27e3585181
Calvin Sauer <calvin.j.sauer@gmail.com>
2018-01-25 12:28:19 -0500

    First crack at a simpler printer double sider calculator

--- src/clj/printer_double_sider/styles.clj
+++ src/clj/printer_double_sider/styles.clj
@@ -1,5 +1,9 @@
 (ns printer-double-sider.styles
-  (:require [garden-watcher.def :refer [defstyles]]))
+  (:require [garden-watcher.def :refer [defstyles]]
+            [garden.units :as u]))
 
 (defstyles style
-  [:h1 {:text-decoration "underline"}])
+  [:.app-container {:display "flex"
+                    :justify-content "center"
+                    :padding-top (u/px 50)}]
+  [:.print-panel {:width (u/px 400)}])
--- src/cljs/printer_double_sider/core.cljs
+++ src/cljs/printer_double_sider/core.cljs
@@ -1,5 +1,6 @@
 (ns printer-double-sider.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [clojure.string :as s]))
 
 (enable-console-print!)
 
@@ -10,13 +11,72 @@
     {:side-one (take-nth 2 ranges)
      :side-two (take-nth 2 (drop 1 ranges))}))
 
-(defonce app-state (atom {:text "Hello Chestnut!"}))
+(defn page-range->str
+  [[lo hi]]
+  (if (= lo hi)
+    (str lo)
+    (s/join "-" [lo hi])))
 
-(defn greeting []
-  [:h1 (:text @app-state)])
+(defn page-range-coll->str
+  [prs]
+  (s/join "," (map page-range->str prs)))
+
+(defonce app-state (atom {:num-pages 30
+                          :slides-per-page 3}))
+
+(defn num-input
+  [{:keys [id label value change-fn]}]
+  [:div.form-group
+   [:label {:for id} label]
+   [:input.form-control.form-control-lg
+    {:id id
+     :type "number"
+     :value (if (js/isNaN value) 0 value)
+     :on-change #(change-fn (try (js/parseInt (.. % -target -value))
+                                 (catch js/Error e 0)))}]])
+
+(defn range-output
+  [{:keys [side-key num-pages slides-per-page]}]
+  (let [range-str (-> (page-ranges num-pages slides-per-page)
+                      side-key
+                      page-range-coll->str)]
+    [:div.form-group
+     [:label
+      {:for (str side-key)}
+      (case side-key
+        :side-one "Side one"
+        :side-two "Side two")]
+     [:textarea.form-control
+      {:value (str range-str)
+       :readOnly true}]]))
+
+(defn print-panel [{:keys [num-pages slides-per-page]}]
+  [:div.print-panel
+   [:h1 "Printer Double Sider"]
+   [:form
+    [num-input {:id "num-pages-input"
+                :label "Number of pages"
+                :value num-pages
+                :change-fn #(swap! app-state assoc :num-pages %)}]
+    [num-input {:id "slides-per-page-input"
+                :label "Slides per page"
+                :value slides-per-page
+                :change-fn #(swap! app-state assoc :slides-per-page %)}]
+    [range-output {:side-key :side-one
+                   :num-pages num-pages
+                   :slides-per-page slides-per-page}]
+    [range-output {:side-key :side-two
+                   :num-pages num-pages
+                   :slides-per-page slides-per-page}]
+    ]])
+
+(defn app
+  []
+  [:div.app-container
+   [print-panel @app-state]])
 
 (defn render []
-  (reagent/render [greeting] (js/document.getElementById "app")))
+  (reagent/render [app] (js/document.getElementById "app")))
 
 (comment
 
